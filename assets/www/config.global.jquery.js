50812d4fb67828e6bca578510d569b70c61f044b
Slashman X <eoin.martin.3007@gmail.com>
2011-08-03 23:24:03 +0100

    Adding basic files

---
+++ assets/www/config.global.jquery.js
@@ -0,0 +1,5 @@
+$(document).bind("mobileinit", function() {
+//	      $.mobile.page.prototype.options.addBackBtn = true;
+//	      $.mobile.defaultPageTransition = 'none';
+//	      $.mobile.useFastClick  = false;
+});
